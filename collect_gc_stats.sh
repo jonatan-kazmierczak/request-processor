db476ed08110b16000a011a7082dbf064387901d
Jonatan Kazmierczak <jonatan.kazmierczak@trivadis.com>
2020-01-28 01:18:55 +0100

    Update collect_gc_stats.sh

--- collect_gc_stats.sh
+++ collect_gc_stats.sh
@@ -18,7 +18,7 @@ JAVA_J9_CMD=/opt2/jdk-13.0.1+9_openj9/bin/java
 JAVA_ZING_CMD=/opt2/zing-jdk11.0.0-19.10.1.0-3/bin/java
 
 echo "-- Java GC Performance Statistics Collector --"
-echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your laptop works in performance mode"
+echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your CPU runs on a constant frequency"
 echo
 
 $JAVA_CMD -version
@@ -31,55 +31,55 @@ mkdir -p $STATS_DIR
 # HotSpot
 
 echo -n vm=HotSpot,gc=Serial, > $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseSerialGC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseSerialGC $JAVA_APP  2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Serial.csv
 
 echo -n vm=HotSpot,gc=Parallel, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseParallelGC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseParallelGC $JAVA_APP  2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Parallel.csv
 
 echo -n vm=HotSpot,gc=G1, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseG1GC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseG1GC $JAVA_APP  2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/G1.csv
 
 echo -n vm=HotSpot,gc=Z, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Z.csv
 
 echo -n vm=HotSpot,gc=Shenandoah, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Shenandoah.csv
 
 # OpenJ9
 
 echo -n vm=OpenJ9,gc=gencon, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/gencon.csv
 
 echo -n vm=OpenJ9,gc=balanced, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/balanced.csv
 
 echo -n vm=OpenJ9,gc=optavgpause, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/optavgpause.csv
 
 echo -n vm=OpenJ9,gc=optthruput, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/optthruput.csv
 
 echo -n vm=OpenJ9,gc=metronome, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/metronome.csv
 
 # Zing
 
 echo -n vm=Zing,gc=C4, >> $OUT_FILE
-/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/C4.csv
 
 # SVM
 
 echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
-/bin/time ./request-processor $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT 2>>$OUT_FILE
+/usr/bin/time ./request-processor $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/SVM.csv
