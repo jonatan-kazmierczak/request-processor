cae7974a85334d20a4a1dabf47c5b0b94e458c9e
Jonatan Kazmierczak <jonatan.kazmierczak@trivadis.com>
2021-01-02 23:06:32 +0100

    Update collect_*_stats.sh

--- collect_gc_stats.sh
+++ collect_gc_stats.sh
@@ -10,12 +10,14 @@ RESPONSE_COUNT=99
 THREAD_COUNT=3
 JAVA_APP="-jar build/libs/request-processor.jar $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT"
 RESPONSE_TIMES_FILE=response_times.txt
-# Path to java command from OpenJDK build including Shenandoah GC - i.e. AdoptOpenJDK
-JAVA_CMD=/opt2/jdk-13.0.1+9/bin/java
+# Path to java command from OpenJDK build
+JAVA_CMD=/opt2/jdk-15.0.1+9_hotspot/bin/java
 # Path to java command from OpenJDK with OpenJ9 VM
-JAVA_J9_CMD=/opt2/jdk-13.0.1+9_openj9/bin/java
+JAVA_J9_CMD=/opt2/jdk-15.0.1+9_openj9/bin/java
 # Path to java command from Zing VM
-JAVA_ZING_CMD=/opt2/zing-jdk11.0.0-19.10.1.0-3/bin/java
+JAVA_ZING_CMD=/opt2/zing20.12.0.0-5-ca-jdk11.0.9-linux_x64/bin/java
+# Path to java command from GraalVM
+JAVA_GRAALVM_CMD=/opt2/graalvm-ce-java11-20.3.0/bin/java
 
 echo "-- Java GC Performance Statistics Collector --"
 echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your CPU runs on a constant frequency"
@@ -31,23 +33,23 @@ mkdir -p $STATS_DIR
 # HotSpot
 
 echo -n vm=HotSpot,gc=Serial, > $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseSerialGC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseSerialGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Serial.csv
 
 echo -n vm=HotSpot,gc=Parallel, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseParallelGC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseParallelGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Parallel.csv
 
 echo -n vm=HotSpot,gc=G1, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseG1GC $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseG1GC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/G1.csv
 
 echo -n vm=HotSpot,gc=Z, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseZGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Z.csv
 
 echo -n vm=HotSpot,gc=Shenandoah, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC $JAVA_APP 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseShenandoahGC $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/Shenandoah.csv
 
 # OpenJ9
@@ -73,13 +75,16 @@ echo -n vm=OpenJ9,gc=metronome, >> $OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/metronome.csv
 
 # Zing
-
 echo -n vm=Zing,gc=C4, >> $OUT_FILE
 /usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/C4.csv
 
-# SVM
+# GraalVM
+echo -n vm=GraalVM,gc=G1, >> $OUT_FILE
+/usr/bin/time $JAVA_GRAALVM_CMD $JAVA_APP 2>>$OUT_FILE
+mv $RESPONSE_TIMES_FILE $STATS_DIR/GraalVM-G1.csv
 
-echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
+# Substrate
+echo -n vm=Substrate,gc=none, >> $OUT_FILE
 /usr/bin/time ./request-processor $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/SVM.csv
--- collect_jvm_stats.sh
+++ collect_jvm_stats.sh
@@ -11,11 +11,11 @@ THREAD_COUNT=3
 JAVA_APP="-jar build/libs/request-processor.jar $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT"
 RESPONSE_TIMES_FILE=response_times.txt
 # Path to java command from OpenJDK build
-JAVA_CMD=java
+JAVA_CMD=/opt2/jdk-15.0.1+9_hotspot/bin/java
 # Path to java command from OpenJDK with OpenJ9 VM
 JAVA_J9_CMD=/opt2/jdk-15.0.1+9_openj9/bin/java
 # Path to java command from Zing VM
-JAVA_ZING_CMD=/opt2/zing20.10.0.0-4-ca-jdk11.0.8.0.101-linux_x64/bin/java
+JAVA_ZING_CMD=/opt2/zing20.12.0.0-5-ca-jdk11.0.9-linux_x64/bin/java
 # Path to java command from GraalVM
 JAVA_GRAALVM_CMD=/opt2/graalvm-ce-java11-20.3.0/bin/java
 
@@ -32,7 +32,7 @@ mkdir -p $STATS_DIR
 
 # HotSpot
 echo -n vm=HotSpot,gc=G1, > $OUT_FILE
-/usr/bin/time $JAVA_CMD $JAVA_APP  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/G1.csv
 
 # OpenJ9
@@ -41,16 +41,16 @@ echo -n vm=OpenJ9,gc=gencon, >> $OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/gencon.csv
 
 # Zing
-#echo -n vm=Zing,gc=C4, >> $OUT_FILE
-#/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
-#mv $RESPONSE_TIMES_FILE $STATS_DIR/C4.csv
+echo -n vm=Zing,gc=C4, >> $OUT_FILE
+/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
+mv $RESPONSE_TIMES_FILE $STATS_DIR/C4.csv
 
 # HotSpot (GraalVM)
-echo -n vm=Graal,gc=G1, >> $OUT_FILE
+echo -n vm=GraalVM,gc=G1, >> $OUT_FILE
 /usr/bin/time $JAVA_GRAALVM_CMD $JAVA_APP 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/GraalVM-G1.csv
 
 # Substrate
-echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
+echo -n vm=Substrate,gc=none, >> $OUT_FILE
 /usr/bin/time ./request-processor $RESPONSE_SIZE $RESPONSE_COUNT $THREAD_COUNT 2>>$OUT_FILE
 mv $RESPONSE_TIMES_FILE $STATS_DIR/SVM.csv
